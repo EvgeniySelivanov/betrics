45a5bb8a6f787806f3a5b8177ba888ebb3f84e1e
Evgeniy Selivanov <genyaselivanovzp@gmail.com>
2023-09-28 15:03:18 +0300

    add goal mechanic

--- components/Goal.jsx
+++ components/Goal.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import {View,ImageBackground} from 'react-native';
+import { View, Text } from 'react-native';
 import styled from 'styled-components/native';
 import { CONSTANTS } from '../constants';
-const bgImage = require('../assets/bgMenu.png');
-const GoalImage = styled(ImageBackground)`
- position:absolute;
- top:${CONSTANTS.GOAL_POSITION.y}px;
- left:${CONSTANTS.GOAL_POSITION.x}px;
- width:${CONSTANTS.GOAL_WIDTH}px;
- height:${CONSTANTS.GOAL_HEIGHT}px;
- overflow: hidden;
+
+const Container = styled.View`
+  position: absolute;
+  top: ${CONSTANTS.GOAL_POSITION.y}px;
+  left: ${CONSTANTS.GOAL_POSITION.x}px;
+  width: ${CONSTANTS.GOAL_WIDTH}px;
+  height: ${CONSTANTS.GOAL_HEIGHT}px;
+  border: 2px solid white;
+  overflow: hidden;
+`;
+const TextGoal = styled.Text`
+  font-size: 30px;
+  font-weight: 700;
+  text-align: center;
+  color: white;
+`;
+const GoalImage = styled.Image`
+  width: 100%;
+  height: 100%;
 `;
 const Goal = () => {
   return (
-    <GoalImage source={bgImage}>
-      
-    </GoalImage>
+    <Container>
+      <TextGoal>GOAL</TextGoal>
+    </Container>
   );
-}
-
-
+};
 
 export default Goal;
--- helpers/AppStateContext.jsx
+++ helpers/AppStateContext.jsx
@@ -3,14 +3,21 @@ import { CONSTANTS } from '../constants';
 export const AppStateContext = createContext();
 export const AppStateProvider = ({ children }) => {
   const [level, setLevelUp] = useState(1);
+  const [isGameRun, setIsGameRun] = useState(false);
 
   const updateLevel = (newData) => {
     setLevelUp(newData);
   };
+  const updateGame = (newData) => {
+    console.log('context run');
+    setIsGameRun(newData);
+  };
 
   const contextValue = {
     level:level,
-    setLevelUp:setLevelUp,
+    updateLevel:updateLevel,
+    isGameRun:isGameRun,
+    updateGame:updateGame,
   };
 
   return (
